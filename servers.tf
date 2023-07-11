f818f7a8165428cac743501de4273d7fac9570ff
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-11 20:23:27 +0530

    \\

--- servers.tf
+++ servers.tf
@@ -68,16 +68,17 @@ resource "aws_instance" "instance" {
   }
 }
 
-/* resource "aws_route53_record" "frontend" {
+ resource "aws_route53_record" "records" {
+   for_each = var.components
   zone_id = "Z104560831NEF0T6OKT9Q"
-  name    = "frontend-dev.anjidevops72.online"
+  name    = "${each.value["name"]}-dev.anjidevops72.online"
   type    = "A"
   ttl     = 300
-  records  = [aws_instance.frontend.private_ip]
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
 }
 
 
-resource "aws_instance" "mongodb" {
+/*resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids =[data.aws_security_group.allow-all.id]

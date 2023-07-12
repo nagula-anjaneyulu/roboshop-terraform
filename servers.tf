4043e69bfc0e84fd1c8d1c83d26e1cdc9162941d
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-12 22:15:00 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   for_each               = { "component" = var.components }
   ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value ["instance_type"]
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {

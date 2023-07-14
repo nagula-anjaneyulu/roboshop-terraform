81f2fda1062bb64b5ab354f4af52fd451bb517d7
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-15 00:25:21 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -25,7 +25,7 @@
          "rm -rf roboshop-shell",
          "git clone https://github.com/nagula-anjaneyulu/roboshop-shell",
          "cd roboshop-shell",
-         "sudo bash ${each.value["name"]}.sh"
+         "sudo bash ${each.value["name"]}.sh ${each.value["password"]}"
        ]
      }
    }

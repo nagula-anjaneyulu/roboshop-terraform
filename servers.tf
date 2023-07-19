d914bb507ec02d488beb89bae904974f356ee909
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-07-19 17:36:18 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,10 +1,7 @@
 module "servers" {
-  for_each = var.components
   source = "./module"
-
   component_name    = each.value["name"]
   env           = var.env
   instance_type = each.value["instance_type"]
-
   password      = lookup(each.value,"password","null")
 }
